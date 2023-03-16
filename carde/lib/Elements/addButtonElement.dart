381eeb3286746799ff68836c71866793d9dd2897
Jiahao <jw3844@drexel.edu>
2023-03-15 22:50:55 -0400

    added buttons to go to new page and text box

--- carde/lib/Elements/addButtonElement.dart
+++ carde/lib/Elements/addButtonElement.dart
@@ -8,9 +8,9 @@ class AddButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-            padding: EdgeInsets.all(60.0),
+            padding: EdgeInsets.all(80.0),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 Container(
                   padding: EdgeInsets.all(20),
@@ -20,6 +20,62 @@ class AddButton extends StatelessWidget {
                   ),
                   child: Icon(
                     Icons.add_card_rounded,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          );
+  }
+}
+
+class NextPage extends StatelessWidget {
+  const NextPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+            padding: EdgeInsets.all(0.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 10.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(10.0)
+                  ),
+                  child: Icon(
+                    Icons.arrow_forward,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          );
+  }
+}
+
+class BackPage extends StatelessWidget {
+  const BackPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+            padding: EdgeInsets.all(0.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 10.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(10.0)
+                  ),
+                  child: Icon(
+                    Icons.arrow_back,
                     color: Colors.black,
                   ),
                 ),
--- carde/lib/Pages/homePage.dart
+++ carde/lib/Pages/homePage.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, prefer_final_fields, unused_field, avoid_unnecessary_containers
 
 import 'package:carde/Elements/addButtonElement.dart';
 import 'package:carde/Elements/cardElement.dart';
@@ -12,7 +12,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  // Necessary for list of cards to work
+  // Necessary for list of cards to works
   PageController _cardController = PageController(initialPage: 0);
   int _activeCard = 0;
   List<Widget> _cards = [];   // Creation of card list
@@ -103,8 +103,68 @@ class _HomePageState extends State<HomePage> {
                 });
               },
               child: AddButton()),
+          //next page button
+        GestureDetector(    // Gesture detector that reacts to taps
+              onTap:() {
+                Navigator.of(context)
+                .push(
+                  MaterialPageRoute(
+                    builder: (context) => Page2()
+              )
+            );
+              },
+              child: NextPage()),
+
         ]),
       ),
     );
   }
 }
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      backgroundColor: Color.fromARGB(255, 44, 49, 58),
+      body: SafeArea(
+        child: Column(children: [
+          GestureDetector(    // Gesture detector that reacts to taps
+              onTap:() {
+                Navigator.of(context)
+                .pop(
+            );
+              },
+              child: BackPage()),
+TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.teal,
+              border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.circular(50)
+              ),
+            ),
+          )
+        ])
+      )
+
+
+
+
+      /*body: SafeArea(
+        onTap: () {
+          Navigator.of(context)
+          .pop();
+
+      },
+      child: NextPage(),
+      )) */
+    );
+    // TODO: implement build
+    //throw UnimplementedError();
+  }
+
+}
+
+
