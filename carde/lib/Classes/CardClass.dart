088ba3e6301c6f95a5a2771e2a2fae224caf5a6e
Bill <billnyesmail@gmail.com>
2023-04-12 21:08:59 -0400

    Created a class for creating cards. The class currently has four attributes, one for each text body on the card. Name, number, email, and other.

---
+++ carde/lib/Classes/CardClass.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Card {
+
+  String? name;
+  String? number;
+  String? email;
+  String? other;
+
+  Card(String name, String number, String email, String other){
+    this.name = name;
+    this.number = number;
+    this.email = email;
+    this.other = other;
+  }
+
+}
+
+  
