a7e65850c34d231699db9efe05f925f88bfeb67f
Bill <billnyesmail@gmail.com>
2023-04-16 00:16:07 -0400

    Got the card class to work.
    I deleted the class I made separately last week.
    I changed the existing class (BlankCard) into a stateless widget.
    I copied the setter methods for the old class into BlankCard.
    I rewrote the code on the editPage to reflect these changes.
    The edit page now works like before but with a variable containing an instantiated object for the display.

---
+++ carde/lib/Elements/BlankCard.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class BlankCard extends StatelessWidget {
+  String name;
+  String number;
+  String email;
+  String bio;
+
+  BlankCard(
+      {super.key,
+      required this.name,
+      required this.number,
+      required this.email,
+      required this.bio});
+
+  editName(newName) {
+    this.name = newName;
+  }
+
+  editNumber(newNumber) {
+    this.number = newNumber;
+  }
+
+  editEmail(newEmail) {
+    this.email = newEmail;
+  }
+
+  editBio(newBio) {
+    this.bio = newBio;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      child: Expanded(
+        child: AspectRatio(
+          aspectRatio: 1.59,
+          child: Container(
+              child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Container(
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20)),
+                // Stuff Within The Card
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    // Text Within The Card
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(this.name,
+                                  style: TextStyle(
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.bold)),
+                              Text(this.number,
+                                  style: TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.bold)),
+                              Text(this.email,
+                                  style: TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.bold)),
+                              Text(this.bio, style: TextStyle(fontSize: 10))
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                    // Picture Within The Card
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Container(
+                          width: 130,
+                          height: 175,
+                          decoration: BoxDecoration(
+                              color: Colors.grey,
+                              borderRadius: BorderRadius.circular(20))),
+                    )
+                  ],
+                )),
+          )),
+        ),
+      ),
+    );
+  }
+}
--- carde/lib/Pages/editPage.dart
+++ carde/lib/Pages/editPage.dart
@@ -1,8 +1,12 @@
+import 'package:carde/Elements/BlankCard.dart';
 import 'package:carde/Elements/PlaceHolderCard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'dart:math' as math;
+import '../Elements/AddButton.dart';
+
+void main() {}
 
 class editPage extends StatefulWidget {
   const editPage({super.key});
@@ -12,15 +16,16 @@ class editPage extends StatefulWidget {
 }
 
 class _editPageState extends State<editPage> {
-  // Variables for validation
-  final formKey = GlobalKey<FormState>();
-  String name = "";
-  String number = "";
-  String email = "";
-  // Variables for displaying text
-  String printName = "";
-  String printNumber = "";
-  String printEmail = "";
+  String userName = "";
+  String userNumber = "";
+  String userEmail = "";
+  String userBio = "";
+  var userCard;
+  _editPageState() {
+    BlankCard createCard = BlankCard(
+        name: userName, number: userNumber, email: userEmail, bio: userBio);
+    this.userCard = createCard;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,108 +63,39 @@ class _editPageState extends State<editPage> {
       Expanded(
         child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
           // The Edit Card
+
+          userCard.build(context),
+
+          // Text Fields
           Padding(
-            padding: const EdgeInsets.all(25),
-            child: Expanded(
-              child: AspectRatio(
-                aspectRatio: 1.59,
-                child: Container(
-                    child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: Container(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 25, horizontal: 35),
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(20)),
-                      // Stuff Within The Card
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          // Text Within The Card
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding: EdgeInsets.all(8.0),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(printName,
-                                        style: TextStyle(
-                                            fontSize: 30,
-                                            fontWeight: FontWeight.bold)),
-                                    Text(printNumber,
-                                        style: TextStyle(
-                                            fontSize: 17,
-                                            fontWeight: FontWeight.bold)),
-                                    Text(printEmail,
-                                        style: TextStyle(
-                                            fontSize: 17,
-                                            fontWeight: FontWeight.bold)),
-                                  ],
-                                ),
-                              )
-                            ],
-                          ),
-                          // Picture Within The Card
-                          Container(
-                              width: 120,
-                              height: 150,
-                              decoration: BoxDecoration(
-                                  color: Colors.grey,
-                                  borderRadius: BorderRadius.circular(20)))
-                        ],
-                      )),
-                )),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(30.0),
+            padding: const EdgeInsets.all(25.0),
             child: Column(
               children: [
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: TextFormField(
-                      maxLength: 15,
                       decoration: InputDecoration(
                           labelText: "Name",
                           filled: true,
                           fillColor: Colors.white),
-                      validator: (value) {
-                        if (value!.isEmpty ||
-                            RegExp(r'^[a-z A-Z]+$').hasMatch(value!)) {
-                          return "Please enter a valid name";
-                        } else {
-                          return null;
-                        }
-                      },
                       onChanged: (value) {
                         setState(() {
-                          printName = value;
+                          userCard.editName(value);
+                          userCard.build(context);
                         });
                       }),
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: TextFormField(
-                      maxLength: 10,
                       decoration: InputDecoration(
                           labelText: "Phone Number",
                           filled: true,
                           fillColor: Colors.white),
-                      validator: (value) {
-                        if (value!.isEmpty ||
-                            RegExp(r'^[0-9]+$').hasMatch(value!)) {
-                          return "Please enter a valid number";
-                        } else {
-                          return null;
-                        }
-                      },
                       onChanged: (value) {
                         setState(() {
-                          printNumber = value;
+                          userCard.editNumber(value);
+                          userCard.build(context);
                         });
                       }),
                 ),
@@ -170,24 +106,31 @@ class _editPageState extends State<editPage> {
                           labelText: "Email",
                           filled: true,
                           fillColor: Colors.white),
-                      validator: (value) {
-                        if (value!.isEmpty ||
-                            !RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}')
-                                .hasMatch(value!)) {
-                          return "Please enter a valid email address";
-                        } else {
-                          return null;
-                        }
-                      },
                       onChanged: (value) {
                         setState(() {
-                          printEmail = value;
+                          userCard.editEmail(value);
+                          userCard.build(context);
+                        });
+                      }),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                      decoration: InputDecoration(
+                          labelText: "Description",
+                          filled: true,
+                          fillColor: Colors.white),
+                      onChanged: (value) {
+                        setState(() {
+                          userCard.editBio(value);
+                          userCard.build(context);
                         });
                       }),
                 )
               ],
             ),
-          )
+          ),
+          // AddButton(),
         ]),
       ),
     ]);
