f0369ec825b64358972752cc9ca8613b02eadd93
Bill Liu <bl698@drexel.edu>
2023-05-04 17:24:37 +0000

    Revert "Merge branch 'zhixian' into 'main'"
    This reverts merge request !19

--- carde/lib/Elements/AddButton.dart
+++ carde/lib/Elements/AddButton.dart
@@ -1,4 +1,3 @@
-
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
@@ -9,23 +8,23 @@ class AddButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-            padding: EdgeInsets.all(60.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle,
-                  ),
-                  child: Icon(
-                    Icons.add_card_rounded,
-                    color: Colors.black,
-                  ),
-                ),
-              ],
+      padding: EdgeInsets.all(60.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              shape: BoxShape.circle,
             ),
-          );
+            child: Icon(
+              Icons.add_card_rounded,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
