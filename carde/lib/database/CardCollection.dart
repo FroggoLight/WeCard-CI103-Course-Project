4a44e0088706c042da0faa9fe8b84efbc5e8ff5b
Bill <billnyesmail@gmail.com>
2023-05-08 22:37:41 -0400

    Finished CRUD operations for two more tables Finished writing the CRUD operations for the UserInfo table and the CardCollection table Also wrote the code CardCollection table and its class

--- carde/lib/database/CardCollection.dart
+++ carde/lib/database/CardCollection.dart
@@ -1,8 +1,35 @@
-// Class containing individual user's collection of cards
-// Contains: List containing other user' card information (from CardInfo class)
-  // TODO:
-  // Initialize class
-  // Initialize list variable
-  // Create getter method that returns the entire list
-  // Create setter method that appends a CardInfo object to that list
-  // Create setter method that deletes a CardInfo object from that list
+import 'package:carde/database/CardInfo.dart';
+
+class CardCollection {
+  final int? id;
+  List<int> cardList;
+  CardCollection({this.id, this.cardList = const []});
+
+  static CardCollection fromJson(Map<String, Object?> json) => CardCollection(
+      id: json[CardCollectionFields.id] as int?,
+      cardList: json[CardCollectionFields.cardList] as List<int>);
+
+  Map<String, Object?> toJson() => {
+        CardCollectionFields.id: id,
+        CardCollectionFields.cardList: cardList,
+      };
+
+  CardCollection copy({
+    int? id,
+    List<int>? cardList,
+  }) =>
+      CardCollection(
+        id: id ?? this.id,
+        cardList: cardList ?? this.cardList,
+      );
+}
+
+// Table containing User Info
+final String cardCollectionTable = 'cardCollection';
+
+// Class for user info fields
+class CardCollectionFields {
+  static final List<String> values = [id, cardList];
+  static final String id = '_id';
+  static String cardList = 'cardList';
+}
